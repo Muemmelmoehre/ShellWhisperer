394a1855ea5272ff6db33f5717643941f9545577
Muemmelmoehre <muemmelmoehre@protonmail.ch>
2020-03-21 17:42:33 -0400

    add vim and nano

--- shw.py
+++ shw.py
@@ -3,7 +3,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Shell cheat sheet. Look up that one-liner you always forget...")
-parser.add_argument(dest="lang", metavar="lang", help="The language in which you'd like to spawn a shell. One-liners are available for bash, perl, python, ruby.", choices=["bash", "perl", "python", "ruby"])
+parser.add_argument(dest="lang", metavar="lang", help="The language or program in which you'd like to spawn a shell. One-liners are available for bash, nano, perl, python, ruby, vim.", choices=["bash", "nano", "perl", "python", "ruby", "vim"])
 args = parser.parse_args()
 
 
@@ -18,3 +18,9 @@ elif(args.lang=="python"):
 
 elif(args.lang=="ruby"):
 	print("exec \"/bin/bash\"")
+
+elif(args.lang=="nano"):
+	print("^R ^X\nreset; sh 1>&0 2>&0")
+
+elif(args.lang=="vim"):
+	print(":!bash \nor\n:set shell=/bin/bash:shell")
